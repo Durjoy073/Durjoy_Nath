307a6c0730ba80bcc47e63fe44049792fbfb1a33
Durjoy Nath <durjoynathcse073@gmail.com>
2023-01-23 00:41:12 +0600

    basic program

---
+++ basic program/array/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+int i,n,s = 0;
+Console.WriteLine("Enter The number element: ");
+n=int.Parse(Console.ReadLine());
+int[] arr = new int[n];
+Console.WriteLine("Enter The Element of  array: ");
+for ( i = 0; i <arr.Length; i++)
+{
+    arr[i]=int.Parse(Console.ReadLine());
+}
+
+for ( i = 0; i <arr.Length; i++)
+{
+    s+= arr[i];
+}
+Console.WriteLine("The sum of array is: "+s);
