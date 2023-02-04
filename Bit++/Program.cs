13fc86c72d26abbc639cd0db2d63e968893152a4
Durjoy Nath <durjoynathcse073@gmail.com>
2023-02-04 22:15:23 +0600

    Bit++ problem solution

---
+++ Bit++/Program.cs
@@ -0,0 +1,16 @@
+﻿
+using System.Security.Cryptography.X509Certificates;
+
+int n,X=0;
+String s; ;
+n=int.Parse(Console.ReadLine());
+for(int i=0; i < n; i++)
+{
+    s=Console.ReadLine();
+
+    if (s[1] =='+')
+        X++;
+    else
+        X--;
+}
+Console.WriteLine(X);
