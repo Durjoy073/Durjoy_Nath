12f46113d6f74cb48fed00bf52268883fb102f0d
Durjoy Nath <durjoynathcse073@gmail.com>
2023-02-04 20:54:29 +0600

    Way too long problem solution

---
+++ Way Too Long Words/Program.cs
@@ -0,0 +1,16 @@
+﻿int n=int.Parse(Console.ReadLine());
+int i;
+string s;
+for (i=0;i<n;i++)
+{
+    s=Console.ReadLine();
+
+    int w = s.Length;
+
+    if (w>10)
+    {
+        Console.WriteLine(s[0]+""+(w - 2) +""+(s[w-1]));
+    }
+    else
+        Console.WriteLine(s);
+}
