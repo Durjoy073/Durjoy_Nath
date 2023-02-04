67a8cdc23c5d71b0d70aa2ecb811917a3a7c7879
Durjoy Nath <durjoynathcse073@gmail.com>
2023-02-04 22:59:15 +0600

    Petya and String Solution Added

---
+++ Petya and string/Program.cs
@@ -0,0 +1,12 @@
+﻿String a, b;
+a=Console.ReadLine();
+a = a.ToLower();
+b=Console.ReadLine();
+b = b.ToLower();
+
+if (a.CompareTo(b) < 0)
+    Console.WriteLine("-1");
+else if (a.CompareTo(b) > 0)
+    Console.WriteLine("1");
+else
+    Console.WriteLine("0");
