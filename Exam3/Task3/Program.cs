63213b0f1a8586ca7aaf4ef902f59d37777d472c
Durjoy Nath <durjoynathcse073@gmail.com>
2023-03-13 22:30:22 +0600

    Task2 solved

--- Exam3/Task3/Program.cs
+++ Exam3/Task3/Program.cs
@@ -1,2 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+DateTime date= DateTime.Now;
+DateTime date2 = new DateTime(2023,3,13);
+date2=date2.AddDays(100);
+
+Console.WriteLine(date2);
