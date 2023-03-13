b58d67ab7db3cfd47178b2fad9b0a5939ce3f537
Durjoy Nath <durjoynathcse073@gmail.com>
2023-03-13 22:50:52 +0600

    Task4 solved

--- Exam3/Task4/Program.cs
+++ Exam3/Task4/Program.cs
@@ -1,2 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Text;
+
 Console.WriteLine("Hello, World!");
+Console.WriteLine("Enter a String: ");
+string s=Console.ReadLine();
+
+StringBuilder b= new StringBuilder(s);
+
+b.Replace('a','-');
+b.Replace('e', '-');
+b.Replace('i', '-');
+b.Replace('o', '-');
+b.Replace('u', '-');
+
+
+Console.WriteLine(b.ToString());
