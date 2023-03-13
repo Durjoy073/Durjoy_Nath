4011e7c0de788974125ef775ee0eb6332a1eb2e0
Durjoy Nath <durjoynathcse073@gmail.com>
2023-03-13 23:22:08 +0600

    Task 10

---
+++ Exam3/Task10/Grid.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task10
+{
+   public class Grid<D,T>
+        where D : class
+       where T : struct
+    {
+        public D Name { get; set; }
+        public T Id { get; set; }
+    }
+}
