6dd2246e164c0f41a8f6c9dd1e5e84824e9e0158
Durjoy Nath <durjoynathcse073@gmail.com>
2023-04-06 21:49:21 +0600

    Initially added

---
+++ Assignment 3/Final Project/Final Project/Program.cs
@@ -0,0 +1,48 @@
+﻿using Final_Project;
+
+if (!Directory.Exists("UserFiles"))
+{
+Directory.CreateDirectory("UserFiles");
+}
+
+
+Console.WriteLine("Please Select data storage:");
+Console.WriteLine("1)Database\n2)File\n3)Exit");
+int n,m;
+n =Convert.ToInt32(Console.ReadLine());
+
+if (n == 1)
+{
+    Console.WriteLine("Please Select task:");
+    Console.WriteLine("1)Create file\n2)Edit file\n3)Delete file\n4)Exit");
+  
+    m = Convert.ToInt32(Console.ReadLine());
+
+    if (m == 1)
+    {
+        ForIFile dcreatefile = new ForIFile();
+        dcreatefile.createFile();
+    }
+    else if (m == 2) { }
+    else if (m == 3) { }
+   
+        
+}
+else if (n == 2)
+{
+    Console.WriteLine("Please Select task:");
+    Console.WriteLine("1)Create file\n2)Edit file\n3)Delete file\n4)Exit");
+
+    m = Convert.ToInt32(Console.ReadLine());
+
+    if (m == 1)
+    {
+
+    }
+    else if (m == 2) { }
+    else if (m == 3) { }
+}
+else if (n == 3)
+{
+    return;
+}
