446c6aff7940981c29de9f5ba5d662005f6f89e6
Durjoy Nath <durjoynathcse073@gmail.com>
2023-04-13 23:44:16 +0600

    added

--- Exam4/Task3/Program.cs
+++ Exam4/Task3/Program.cs
@@ -1,3 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 string[] fruits = { "apple", "banana", "mango", "goava", "strawberry", "pineapple" };
+
+
+var f = fruits.GroupBy(fruit => fruit[fruit.Length - 1]) .OrderBy(group => group.Key);
+var r = f.ToList();
+
+foreach (var group in r)
+{
+    Console.WriteLine("Fruits with last letter"+ group.Key);
+    foreach (var fruit in group)
+    {
+        Console.WriteLine(fruit);
+    }
+}
