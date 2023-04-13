1c3bc6e98011c5051ed1e88d6d6916e0e5aea8f8
Durjoy Nath <durjoynathcse073@gmail.com>
2023-04-13 23:36:44 +0600

    initial

---
+++ Exam4/Task1/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+List<(int id, string courseName, double price)> courses = 
+    new List<(int id, string courseName, double price)>();
+
+courses.Add((1, "C#", 8000));
+courses.Add((2, "Asp.net", 30000));
+courses.Add((3, "Java", 7000));
+courses.Add((4, "Python",10000));
+List<(int id, string name, string address, int courseId)> students = 
+    new List<(int id, string name, string address, int courseId)>();
+students.Add((1, "Jalaluddin", "Mirpur", 2));
+students.Add((2, "Hasan", "Moghbazar", 1));
+students.Add((3, "Nishan", "Feni", 3));
+students.Add((4, "Durjoy", "Chattogram", 4));
+
+var add = students.Join(courses, student => student.courseId, course=> course.id,(student,course)
+    => new {student.name,course.courseName});
+var match = add.ToList();
+foreach(var item in match)
+{
+    Console.WriteLine(" "+item.name+","+ ""+item.courseName);
+}
---
+++ Exam4/Task2/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Text.RegularExpressions;
+
+List<(int id, string name, int age)> persons = new List<(int id, string name, int age)>();
+persons.Add((1, "Jalaluddin", 42));
+persons.Add((2, "Hasan", 32));
+persons.Add((3, "Maruf", 23));
+persons.Add((4, "Hasan", 33));
+
+
+var sort= persons.OrderBy(person=> person.name).ThenBy(person=>person.age);
+
+var s = sort.ToList();
+
+foreach (var person in s)
+{
+    Console.WriteLine(" " + person.name + "," + "" + person.age);
+}
